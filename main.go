210d05f017bdd23eed4dbec91adb8b2b6f4df451
Abhijit Kar <abhijit.kar@derpycoder.com>
2024-06-02 18:52:29 +0530

    Removed unused code, imports and variables.

--- main.go
+++ main.go
@@ -9,27 +9,12 @@ import (
 )
 
 func main() {
-	// app := fiber.New()
-
-	// app.Get("/", func(c *fiber.Ctx) error {
-
-	// 	// component := hello("Abhijit")
-	// 	// component.Render(context.Background(), os.Stdout)
-	// 	c.Set("Content-type", "text/html")
-	// 	return hello("Abhijit").Render(context.Background(), c.Response().BodyWriter())
-
-	// })
-	// app.Get("/", adaptor.HTTPHandler(templ.Handler(templates.Hello("Abhijit"))))
-	// app.Get("/hello", adaptor.HTTPHandler(templ.Handler(templates.Hello("Abhijit Kar"))))
-
-	// app.Listen(":3000")
-
 	f, err := os.Create("build/index.html")
 	if err != nil {
 		log.Fatalf("failed to create output file: %v", err)
 	}
 
-	err = templates.Menu("John").Render(context.Background(), f)
+	err = templates.Menu().Render(context.Background(), f)
 	if err != nil {
 		log.Fatalf("failed to write output file: %v", err)
 	}
