1d9d54fb75c63a6471869f39aa6a042e7a8427c7
Abhijit Kar <abhijit.kar@derpycoder.com>
2024-06-03 10:18:31 +0530

    Scan Me page complete.

--- main.go
+++ main.go
@@ -9,12 +9,21 @@ import (
 )
 
 func main() {
-	f, err := os.Create("build/index.html")
+	index_page, err := os.Create("build/index.html")
 	if err != nil {
 		log.Fatalf("failed to create output file: %v", err)
 	}
 
-	err = templates.Menu().Render(context.Background(), f)
+	scan_me_page, err := os.Create("build/scan-me.html")
+	if err != nil {
+		log.Fatalf("failed to create output file: %v", err)
+	}
+
+	err = templates.Menu().Render(context.Background(), index_page)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
+	}
+	err = templates.QRCode().Render(context.Background(), scan_me_page)
 	if err != nil {
 		log.Fatalf("failed to write output file: %v", err)
 	}
